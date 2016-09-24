d2797403ea199cf1ff88f58438262efecae3c79d
Tyler Smith <tyler.smith@pteraforce.com>
2016-09-24 15:09:40 -0700

    Add social networks table

--- app/models.py
+++ app/models.py
@@ -9,6 +9,15 @@ review_features = db.Table(
 )
 
 
+class SocialNetwork(db.Model):
+    __tablename__ = 'social_networks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(31))
+
+    def __repr__(self):
+        return '<SocialNetwork %r>' % self.name
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.BigInteger, primary_key=True)

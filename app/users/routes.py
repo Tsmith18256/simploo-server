37e9bfce0a10a6e2aa5067c7c02bcc7c9dfb8995
Tyler Smith <tyler.smith@pteraforce.com>
2016-09-24 21:48:32 -0700

    Add get user by ID endpoint

--- app/users/routes.py
+++ app/users/routes.py
@@ -5,13 +5,37 @@ from .. import auth
 from ..models import User
 
 
-def parse_user(u):
-    return {
+def parse_user(u, include_email=True):
+    user = {
         'id': u.id,
-        'email': u.email,
         'first_name': u.first_name,
         'last_name': u.last_name
     }
+
+    if include_email:
+        user['email'] = u.email
+
+    return user
+
+
+@users.route('/<int:id>', methods=['GET'])
+def get_user(id):
+    """
+    @api {get} /users/:id Get user by ID
+    @apiVersion 0.1.0
+    @apiName GetUserById
+    @apiGroup User
+
+    @apiParam {Number} id   The unique ID of the user.
+
+    @apiSuccess {Number} id             The unique ID of the user.
+    @apiSuccess {String} first_name     The first name of the user.
+    @apiSuccess {String} last_name      The last name of the user.
+    """
+    user = User.query.filter_by(id=id).first()
+
+    # email property isn't public, must use /me to see it
+    return jsonify(parse_user(user, include_email=False))
 
 
 @users.route('/me', methods=['GET'])

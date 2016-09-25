065cedf847a5b4043d8a5c84936b5d0dfaa097a2
Tyler Smith <tyler.smith@pteraforce.com>
2016-09-24 18:30:03 -0700

    Add get review by ID endpoint

---
+++ app/reviews/routes.py
@@ -0,0 +1,60 @@
+from flask import jsonify
+
+from . import reviews
+from ..models import Review
+
+
+def parse_review(r):
+    return {
+        'id': r.id,
+        'user_id': r.user_id,
+        'washroom_id': r.washroom_id,
+        'description': r.description,
+        'cleanliness': r.cleanliness,
+        'privacy': r.privacy,
+        'safety': r.safety,
+        'accessibility': r.accessibility,
+        'features': r.features,
+        'rating': r.rating
+    }
+
+
+@reviews.route('/<int:id>', methods=['GET'])
+def get_review_by_id(id):
+    """
+    @api {get} /reviews/:id Get review by ID
+    @apiVersion 0.1.0
+    @apiName GetReviewById
+    @apiGroup Review
+
+    @apiParam {Number} id   The unique ID of the review.
+
+    @apiSuccess {Number}    id              The unique ID of the review.
+    @apiSuccess {Number}    user_id         The ID of the user that wrote the
+                                            review.
+    @apiSuccess {Number}    washroom_id     The ID of the washroom being
+                                            reviewed.
+    @apiSuccess {String}    description     The text body of the user's review.
+    @apiSuccess {Number}    rating          The user's overall rating of the
+                                            washroom.
+    @apiSuccess {Number}    cleanliness     The user's cleanliness rating for
+                                            the washroom.
+    @apiSuccess {Number}    privacy         The user's privacy rating for the
+                                            washroom.
+    @apiSuccess {Number}    safety          The user's safety rating for the
+                                            washroom.
+    @apiSuccess {Number}    accessibility   The user's accessibility rating for
+                                            the washroom.
+    @apiSuccess {Object[]}  features        The features supported by the
+                                            washroom, as selected by the user.
+    """
+    r = Review.query.filter_by(id=id).first()
+
+    if r is not None:
+        return jsonify(parse_review(r))
+    else:
+        return jsonify({
+            'error_type': 'invalid_param',
+            'invalid_param': 'id',
+            'message': 'No reviews were found with the given ID'
+        })
